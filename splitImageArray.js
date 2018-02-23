577495fa0d9c45a4e7be8cd771322a2b49bfeaee
sts6812 <11427113+sts6812@users.noreply.github.com>
2018-02-23 09:56:27 -0500

    Update splitImageArray.js

--- splitImageArray.js
+++ splitImageArray.js
@@ -1,19 +1,21 @@
-"splitImageArray" = [function (require, module, exports) {
+({
+    "splitImageArray": [function (require, module, exports) {
 
-    exports.splitImageArray = function (array) {
-        var count, i, index, layer, len, splitAngle;
-        index = 0;
-        count = array.length;
-        splitAngle = 360 / count;
-        for (i = 0, len = array.length; i < len; i++) {
-            layer = array[i];
-            layer.originX = 0.5;
-            layer.originY = 1.5;
-            layer.rotationZ = splitAngle * index;
-            index++;
-        }
-        return splitAngle;
-    };
+        exports.splitImageArray = function (array) {
+            var count, i, index, layer, len, splitAngle;
+            index = 0;
+            count = array.length;
+            splitAngle = 360 / count;
+            for (i = 0, len = array.length; i < len; i++) {
+                layer = array[i];
+                layer.originX = 0.5;
+                layer.originY = 1.5;
+                layer.rotationZ = splitAngle * index;
+                index++;
+            }
+            return splitAngle;
+        };
 
 
 }, {}]
+}, {}, [])
