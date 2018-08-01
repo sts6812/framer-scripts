f0120ba2012c2329fe769a626d311c06d414f125
sts6812 <11427113+sts6812@users.noreply.github.com>
2018-08-01 10:04:08 -0400

    Create input.js

---
+++ input.js
@@ -0,0 +1,349 @@
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"input":[function(require,module,exports){
+var wrapInput,
+  bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+  extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+  hasProp = {}.hasOwnProperty;
+
+Events.EnterKey = "EnterKey";
+
+Events.SpaceKey = "SpaceKey";
+
+Events.BackspaceKey = "BackspaceKey";
+
+Events.CapsLockKey = "CapsLockKey";
+
+Events.ShiftKey = "ShiftKey";
+
+Events.ValueChange = "ValueChange";
+
+Events.InputFocus = "InputFocus";
+
+Events.InputBlur = "InputBlur";
+
+exports.InputLayer = (function(superClass) {
+  extend(InputLayer, superClass);
+
+  function InputLayer(options) {
+    var base, currentValue, property, textProperties, value;
+    if (options == null) {
+      options = {};
+    }
+    this._setTextProperties = bind(this._setTextProperties, this);
+    this._setPlaceholder = bind(this._setPlaceholder, this);
+    _.defaults(options, {
+      backgroundColor: "#FFF",
+      width: 375,
+      height: 60,
+      padding: {
+        left: 20
+      },
+      text: "Type something...",
+      fontSize: 40,
+      fontWeight: 300
+    });
+    if (options.multiLine) {
+      if ((base = options.padding).top == null) {
+        base.top = 20;
+      }
+    }
+    this._inputElement = document.createElement("input");
+    this._inputElement.style.position = "absolute";
+    InputLayer.__super__.constructor.call(this, options);
+    this._background = void 0;
+    this._placeholder = void 0;
+    this._isDesignLayer = false;
+    this.input = new Layer({
+      backgroundColor: "transparent",
+      name: "input",
+      width: this.width,
+      height: this.height,
+      parent: this
+    });
+    if (this.multiLine) {
+      this._inputElement = document.createElement("textarea");
+    }
+    this.input._element.appendChild(this._inputElement);
+    this._setTextProperties(this);
+    this._inputElement.autocomplete = "off";
+    this._inputElement.autocorrect = "off";
+    this._inputElement.spellcheck = false;
+    this._inputElement.className = "input" + this.id;
+    textProperties = {
+      text: this.text,
+      fontFamily: this.fontFamily,
+      fontSize: this.fontSize,
+      lineHeight: this.lineHeight,
+      fontWeight: this.fontWeight,
+      color: this.color,
+      backgroundColor: this.backgroundColor,
+      width: this.width,
+      height: this.height,
+      padding: this.padding,
+      parent: this.parent
+    };
+    for (property in textProperties) {
+      value = textProperties[property];
+      this.on("change:" + property, (function(_this) {
+        return function(value) {
+          _this._elementHTML.children[0].textContent = "";
+          if (_this._isDesignLayer) {
+            return;
+          }
+          _this._setTextProperties(_this);
+          return _this._setPlaceholderColor(_this._id, _this.color);
+        };
+      })(this));
+    }
+    this._setPlaceholder(this.text);
+    this._setPlaceholderColor(this._id, this.color);
+    this._elementHTML.children[0].textContent = "";
+    this._isFocused = false;
+    this._inputElement.onfocus = (function(_this) {
+      return function(e) {
+        if (_this.focusColor == null) {
+          _this.focusColor = "#000";
+        }
+        _this.emit(Events.InputFocus, event);
+        return _this._isFocused = true;
+      };
+    })(this);
+    this._inputElement.onblur = (function(_this) {
+      return function(e) {
+        _this.emit(Events.InputBlur, event);
+        return _this._isFocused = false;
+      };
+    })(this);
+    currentValue = void 0;
+    this._inputElement.onkeydown = (function(_this) {
+      return function(e) {
+        currentValue = _this.value;
+        if (e.which === 20) {
+          _this.emit(Events.CapsLockKey, event);
+        }
+        if (e.which === 16) {
+          return _this.emit(Events.ShiftKey, event);
+        }
+      };
+    })(this);
+    this._inputElement.onkeyup = (function(_this) {
+      return function(e) {
+        if (currentValue !== _this.value) {
+          _this.emit("change:value", _this.value);
+          _this.emit(Events.ValueChange, _this.value);
+        }
+        if (e.which === 13) {
+          _this.emit(Events.EnterKey, event);
+        }
+        if (e.which === 8) {
+          _this.emit(Events.BackspaceKey, event);
+        }
+        if (e.which === 32) {
+          _this.emit(Events.SpaceKey, event);
+        }
+        if (e.which === 20) {
+          return _this.emit(Events.CapsLockKey, event);
+        }
+      };
+    })(this);
+  }
+
+  InputLayer.prototype._setPlaceholder = function(text) {
+    return this._inputElement.placeholder = text;
+  };
+
+  InputLayer.prototype._setPlaceholderColor = function(id, color) {
+    return document.styleSheets[0].addRule(".input" + id + "::-webkit-input-placeholder", "color: " + color);
+  };
+
+  InputLayer.prototype._checkDevicePixelRatio = function() {
+    var dpr, ratio;
+    ratio = Screen.width / Framer.Device.screen.width;
+    if (Utils.isDesktop()) {
+      if (ratio < 0.5 && ratio > 0.25) {
+        dpr = 1 - ratio;
+      } else if (ratio === 0.25) {
+        dpr = 1 - (ratio * 2);
+      } else {
+        dpr = Utils.devicePixelRatio();
+      }
+      if (Framer.Device.deviceType === "fullscreen") {
+        dpr = 2;
+      }
+    } else {
+      if (ratio < 0.5 && ratio > 0.25) {
+        dpr = 1 - ratio;
+      } else if (ratio === 0.25) {
+        dpr = 1 - (ratio * 2);
+      } else if (ratio === 0.5) {
+        dpr = 1;
+      }
+    }
+    return dpr;
+  };
+
+  InputLayer.prototype._setTextProperties = function(layer) {
+    var dpr, ref;
+    dpr = this._checkDevicePixelRatio();
+    if (!this._isDesignLayer) {
+      this._inputElement.style.fontFamily = layer.fontFamily;
+      this._inputElement.style.fontSize = (layer.fontSize / dpr) + "px";
+      this._inputElement.style.fontWeight = (ref = layer.fontWeight) != null ? ref : "normal";
+      this._inputElement.style.paddingTop = (layer.padding.top * 2 / dpr) + "px";
+      this._inputElement.style.paddingRight = (layer.padding.bottom * 2 / dpr) + "px";
+      this._inputElement.style.paddingBottom = (layer.padding.right * 2 / dpr) + "px";
+      this._inputElement.style.paddingLeft = (layer.padding.left * 2 / dpr) + "px";
+    }
+    this._inputElement.style.width = ((layer.width - layer.padding.left * 2) * 2 / dpr) + "px";
+    this._inputElement.style.height = (layer.height * 2 / dpr) + "px";
+    this._inputElement.style.outline = "none";
+    this._inputElement.style.backgroundColor = "transparent";
+    this._inputElement.style.cursor = "auto";
+    this._inputElement.style.webkitAppearance = "none";
+    this._inputElement.style.resize = "none";
+    this._inputElement.style.overflow = "hidden";
+    return this._inputElement.style.webkitFontSmoothing = "antialiased";
+  };
+
+  InputLayer.prototype.addBackgroundLayer = function(layer) {
+    this._background = layer;
+    this._background.parent = this;
+    this._background.name = "background";
+    this._background.x = this._background.y = 0;
+    this._background._element.appendChild(this._inputElement);
+    return this._background;
+  };
+
+  InputLayer.prototype.addPlaceHolderLayer = function(layer) {
+    var dpr;
+    this._isDesignLayer = true;
+    this._inputElement.className = "input" + layer.id;
+    this.padding = {
+      left: 0,
+      top: 0
+    };
+    this._setPlaceholder(layer.text);
+    this._setTextProperties(layer);
+    this._setPlaceholderColor(layer.id, layer.color);
+    this.on("change:color", (function(_this) {
+      return function() {
+        return _this._setPlaceholderColor(layer.id, _this.color);
+      };
+    })(this));
+    layer.visible = false;
+    this._elementHTML.children[0].textContent = "";
+    dpr = this._checkDevicePixelRatio();
+    this._inputElement.style.fontSize = (layer.fontSize * 2 / dpr) + "px";
+    this._inputElement.style.paddingTop = (layer.y * 2 / dpr) + "px";
+    this._inputElement.style.paddingLeft = (layer.x * 2 / dpr) + "px";
+    this._inputElement.style.width = ((this._background.width - layer.x * 2) * 2 / dpr) + "px";
+    if (this.multiLine) {
+      this._inputElement.style.height = (this._background.height * 2 / dpr) + "px";
+    }
+    this.on("change:padding", (function(_this) {
+      return function() {
+        _this._inputElement.style.paddingTop = (_this.padding.top * 2 / dpr) + "px";
+        return _this._inputElement.style.paddingLeft = (_this.padding.left * 2 / dpr) + "px";
+      };
+    })(this));
+    return this._placeholder;
+  };
+
+  InputLayer.prototype.focus = function() {
+    return this._inputElement.focus();
+  };
+
+  InputLayer.define("value", {
+    get: function() {
+      return this._inputElement.value;
+    },
+    set: function(value) {
+      return this._inputElement.value = value;
+    }
+  });
+
+  InputLayer.define("focusColor", {
+    get: function() {
+      return this._inputElement.style.color;
+    },
+    set: function(value) {
+      return this._inputElement.style.color = value;
+    }
+  });
+
+  InputLayer.define("multiLine", InputLayer.simpleProperty("multiLine", false));
+
+  InputLayer.wrap = function(background, placeholder, options) {
+    return wrapInput(new this(options), background, placeholder, options);
+  };
+
+  InputLayer.prototype.onEnterKey = function(cb) {
+    return this.on(Events.EnterKey, cb);
+  };
+
+  InputLayer.prototype.onSpaceKey = function(cb) {
+    return this.on(Events.SpaceKey, cb);
+  };
+
+  InputLayer.prototype.onBackspaceKey = function(cb) {
+    return this.on(Events.BackspaceKey, cb);
+  };
+
+  InputLayer.prototype.onCapsLockKey = function(cb) {
+    return this.on(Events.CapsLockKey, cb);
+  };
+
+  InputLayer.prototype.onShiftKey = function(cb) {
+    return this.on(Events.ShiftKey, cb);
+  };
+
+  InputLayer.prototype.onValueChange = function(cb) {
+    return this.on(Events.ValueChange, cb);
+  };
+
+  InputLayer.prototype.onInputFocus = function(cb) {
+    return this.on(Events.InputFocus, cb);
+  };
+
+  InputLayer.prototype.onInputBlur = function(cb) {
+    return this.on(Events.InputBlur, cb);
+  };
+
+  return InputLayer;
+
+})(TextLayer);
+
+wrapInput = function(instance, background, placeholder) {
+  var input, ref;
+  if (!(background instanceof Layer)) {
+    throw new Error("InputLayer expects a background layer.");
+  }
+  if (!(placeholder instanceof TextLayer)) {
+    throw new Error("InputLayer expects a text layer.");
+  }
+  input = instance;
+  if (input.__framerInstanceInfo == null) {
+    input.__framerInstanceInfo = {};
+  }
+  if ((ref = input.__framerInstanceInfo) != null) {
+    ref.name = instance.constructor.name;
+  }
+  input.frame = background.frame;
+  input.parent = background.parent;
+  input.index = background.index;
+  input.addBackgroundLayer(background);
+  input.addPlaceHolderLayer(placeholder);
+  return input;
+};
+
+
+},{}],"myModule":[function(require,module,exports){
+exports.myVar = "myVariable";
+
+exports.myFunction = function() {
+  return print("myFunction is running");
+};
+
+exports.myArray = [1, 2, 3];
+
+
+},{}]},{},[])
